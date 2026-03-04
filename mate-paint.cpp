36089092a2feee971a95df393d6a0a372e101892
DMJC <jcarthew@gmail.com>
2026-03-04 11:53:36 +1100

    Transparency Canvas patch. Stops widget becoming visible.

--- mate-paint.cpp
+++ mate-paint.cpp
@@ -59,6 +59,7 @@ void update_zoom_buttons();
 void update_zoom_visibility();
 void push_undo_state();
 void undo_last_operation();
+void draw_canvas_grid_background(cairo_t* cr);
 bool is_transparent_color(const GdkRGBA& color);
 bool save_surface_to_file(cairo_surface_t* surface, const std::string& filename);
 void load_custom_palette_colors();
@@ -1899,12 +1900,46 @@ void draw_preview(cairo_t* cr) {
     cairo_restore(cr);
 }
 
+void draw_canvas_grid_background(cairo_t* cr) {
+    static cairo_pattern_t* checker_pattern = nullptr;
+
+    if (!checker_pattern) {
+        cairo_surface_t* pattern_surface = cairo_image_surface_create(CAIRO_FORMAT_RGB24, 2, 2);
+        cairo_t* pattern_cr = cairo_create(pattern_surface);
+        configure_crisp_rendering(pattern_cr);
+
+        cairo_set_source_rgb(pattern_cr, 1.0, 1.0, 1.0);
+        cairo_paint(pattern_cr);
+
+        cairo_set_source_rgb(pattern_cr, 0.0, 0.0, 0.0);
+        cairo_rectangle(pattern_cr, 0, 0, 1, 1);
+        cairo_rectangle(pattern_cr, 1, 1, 1, 1);
+        cairo_fill(pattern_cr);
+
+        cairo_destroy(pattern_cr);
+
+        checker_pattern = cairo_pattern_create_for_surface(pattern_surface);
+        cairo_pattern_set_extend(checker_pattern, CAIRO_EXTEND_REPEAT);
+        cairo_pattern_set_filter(checker_pattern, CAIRO_FILTER_NEAREST);
+        cairo_surface_destroy(pattern_surface);
+    }
+
+    cairo_save(cr);
+    cairo_rectangle(cr, 0, 0, app_state.canvas_width, app_state.canvas_height);
+    cairo_set_source(cr, checker_pattern);
+    cairo_fill(cr);
+    cairo_restore(cr);
+}
+
 // Canvas draw callback
 gboolean on_draw(GtkWidget* widget, cairo_t* cr, gpointer data) {
     if (app_state.surface) {
         configure_crisp_rendering(cr);
         cairo_save(cr);
         cairo_scale(cr, app_state.zoom_factor, app_state.zoom_factor);
+
+        draw_canvas_grid_background(cr);
+
         cairo_set_source_surface(cr, app_state.surface, 0, 0);
         cairo_pattern_set_filter(cairo_get_source(cr), CAIRO_FILTER_NEAREST);
         cairo_paint(cr);
